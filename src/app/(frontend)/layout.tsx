01349bce7df85547ecb9d03ff4669c46a1d344ca
Srijan Srivastava <srijan.srivastava35@gmail.com>
2025-04-05 19:38:39 +0530

    refact: removed fluff, added theme

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
-import './styles.css'
+import './tailwind.css';
 
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
   title: 'Payload Blank Template',
-}
+};
 
 export default async function RootLayout(props: { children: React.ReactNode }) {
-  const { children } = props
+  const { children } = props;
 
   return (
     <html lang="en">
@@ -15,5 +14,5 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
         <main>{children}</main>
       </body>
     </html>
-  )
+  );
 }
