bda6342848415129883221929276d3f52769f9d6
Srijan Srivastava <srijan.srivastava35@gmail.com>
2025-04-07 11:35:27 +0530

    added: shadcn

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,8 +1,11 @@
+import { Button } from '@/components/ui/button';
+
 export default async function HomePage() {
   return (
     <div className="home bg-black-700 min-h-screen flex flex-col @container">
       <div>
-        <h1 className="px-20 text-display-1 text-(--color-brand) px-5">My Website</h1>
+        <h1 className="text-display-1 text-brand px-5">My Website</h1>
+        <Button>Hello</Button>
       </div>
     </div>
   );
